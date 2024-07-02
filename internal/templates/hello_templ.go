7890a82133f8ce228ea241a1f078e0d29815af7f
Dennis von Bargen <dennis@vonbargen.se>
2024-07-02 12:29:30 +0200

    Minor fix in hello template

--- internal/templates/hello_templ.go
+++ internal/templates/hello_templ.go
@@ -39,7 +39,7 @@ func Hello(name string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("!</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
