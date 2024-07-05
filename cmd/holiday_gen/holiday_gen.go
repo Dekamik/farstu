455a1ba5e787292854ce3d3beeffc0bce2dd94ca
Dennis von Bargen <dennis@vonbargen.se>
2024-07-05 13:43:36 +0200

    Created types for sholiday API

---
+++ cmd/holiday_gen/holiday_gen.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strings"
+	"time"
+)
+
+// SBool is a bool that can be parsed from JSON as Ja or Nej
+type SBool bool
+
+func (b *SBool) UnmarshalJSON(data []byte) error {
+	str := string(data)
+	*b = strings.ToLower(str) == "ja"
+	return nil
+}
+
+type SHolidayResponse struct {
+	CacheTime time.Time `json:"cachetid"`
+	Version   string    `json:"version"`
+	URI       string    `json:"uri"`
+	StartDate time.Time `json:"startdatum"`
+	EndDate   time.Time `json:"slutdatum"`
+	Days      struct {
+		Date         time.Time `json:"datum"`
+		Weekday      string    `json:"veckodag"`
+		IsNotWorkday SBool     `json:"arbetsfri dag"`
+		IsRedDay     SBool     `json:"röd dag"`
+		WeekNumber   string    `json:"vecka"`
+		DayInWeek    string    `json:"dag i vecka"`
+		Holiday      string    `json:"helgdag"`
+		NamesDay     []string  `json:"namnsdag"`
+		FlagDay      string    `json:"flaggdag"`
+	} `json:"dagar"`
+}
+
+func main() {
+
+}
